6bb9919447f01dfc5883f598cebe70fd9676d73e
“Kajol7052” <kajol7052@gmail.com>
2020-12-01 10:13:25 +0530

    Added : Check Employee Daily Wages

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -3,19 +3,26 @@
 import java.util.*;
 import java.util.Random;
 
-//UC-1 To check if Employee is Present or Absent
+//UC-1 To Check If Employee is Present or Absent
+//UC-2 To Calculate Daily Employee Wage
 public class EmployeeWages {
         public static void main(String args[]){
                 int IS_FULL_TIME= 1;
+		int RATE_PER_HOUR= 20;
+        	int empHours=0;
                 double empCheck=Math.floor(Math.random()*10)%2;
                 System.out.println(empCheck);
                 if(empCheck == IS_FULL_TIME)
                 {
-                        System.out.println("Present");
+			empHours=8;
+                        //System.out.println("Present");
                 }
                 else
                 {
-                        System.out.println("Absent");
+			empHours=0;
+                        //System.out.println("Absent");
                 }
+		int salary = empHours*RATE_PER_HOUR;
+        	System.out.println("Salary is : " + salary);
         }
 }
