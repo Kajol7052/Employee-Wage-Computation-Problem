d8a79ed4bbdc23d76d5d74d3a08451322b39df74
“Kajol7052” <kajol7052@gmail.com>
2020-12-01 11:18:12 +0530

    Added : Switch Case for Calculating Daily Wages

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -5,23 +5,26 @@ import java.util.Random;
 
 //UC-1 To Check If Employee is Present or Absent
 //UC-2 To Calculate Daily Employee Wage
+//UC-3 To Add Part Time Employee and Wages
+//UC-4 Using Switch Case Statement
 public class EmployeeWages {
         public static void main(String args[]){
                 int IS_FULL_TIME= 1;
+		int IS_PART_TIME= 0;
 		int RATE_PER_HOUR= 20;
         	int empHours=0;
-                double empCheck=Math.floor(Math.random()*10)%2;
+                int empCheck=(int) (Math.floor(Math.random()*10) %2);
                 System.out.println(empCheck);
-                if(empCheck == IS_FULL_TIME)
-                {
-			empHours=8; // FullTime
-                        //System.out.println("Present");
-                }
-                else
-                {
-			empHours=4; // PartTime
-                        //System.out.println("Absent");
-                }
+                switch (empCheck) {
+        	case 1:
+        		empHours=8;
+            		break;
+        	case 0:
+        		empHours=4;
+        		break;
+        	default:
+        		break;
+        }
 		int salary = empHours*RATE_PER_HOUR;
         	System.out.println("Salary is : " + salary);
         }
